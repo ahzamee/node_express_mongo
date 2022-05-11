660fcd5b6640bcbee7480b941e309b1787a1f0f5
zamee <zamee0192@gmail.com>
2022-05-11 18:01:08 +0600

    middleware added

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
 const express = require('express');
 const router = require("./routes");
-
+const middlewares = require('./middlewares');
 const db = require('./database');
 
 require('dotenv').config({ silent: true });
 
 const app = express();
 
+app.use(...middlewares);
+
 db.connectMongoAtlas();
 
 router.registerApplicationRoutes(app);
+
+app.all("*", (req, res, next) => {
+  next(`Cannot find ${req.originalUrl} on this server`, 404);
+});
 
 module.exports = app;
